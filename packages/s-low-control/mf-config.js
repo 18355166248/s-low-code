7e817376bbfc387c0c15eb5d3b1df624fc5c6513
SMegalo <lang.jiang@ximalaya.com>
2023-06-01 19:37:01 +0800

    feat: 打包

--- packages/s-low-control/mf-config.js
+++ packages/s-low-control/mf-config.js
@@ -6,7 +6,7 @@ module.exports = {
   remotes: [
     {
       name: "remote",
-      url: isDev ? "http://localhost:4000/" : "http://110.42.188.221:10001/",
+      url: isDev ? "http://localhost:4000/" : "http://110.42.188.221:11001/",
     },
   ],
   shared: {
