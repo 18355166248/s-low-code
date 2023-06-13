d2b0c1bfdc0572790356fcdddb074e0f6a76e02d
SMegalo <lang.jiang@ximalaya.com>
2023-06-13 19:35:31 +0800

    feat: 远程组件开发10%

--- packages/s-low-control/config/webpack.config.js
+++ packages/s-low-control/config/webpack.config.js
@@ -346,6 +346,9 @@ module.exports = function (webpackEnv) {
         }),
         ...(modules.webpackAliases || {}),
       },
+      fallback: {
+        path: require.resolve("path-browserify"),
+      },
       plugins: [
         // Prevents users from importing files from outside of src/ (or node_modules/).
         // This often causes confusion because we only process files within src/ with babel.
--- packages/s-low-control/src/pages/Editor/comp/Left/index.tsx
+++ packages/s-low-control/src/pages/Editor/comp/Left/index.tsx
@@ -1,7 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import menus from "../../schema/fields";
+import { importRemote } from "@module-federation/utilities";
 
 function Left() {
+  const [remoteList, setRemoteList] = useState([]);
+
+  useEffect(() => {
+    const app2Utils = importRemote({
+      url: "http://localhost:4001",
+      scope: "lowTitle",
+      module: "./fields",
+    });
+    app2Utils.then((res) => {
+      console.log("res", res);
+    });
+  }, []);
+
   return (
     <div style={{ width: 221 }}>
       {menus.map((menu) => {
