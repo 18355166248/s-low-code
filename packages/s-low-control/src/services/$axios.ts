a898fdfbfaa3f5bb34c2e3b85862d2338041d7b3
jianglang <lang.jiang@ximalaya.com>
2023-07-02 21:11:29 +0800

    feat: 优化打包

--- packages/s-low-control/src/services/$axios.ts
+++ packages/s-low-control/src/services/$axios.ts
@@ -4,7 +4,7 @@ import { trimRequestInterceptors } from "./$axios.config";
 import { getToken, removeAuthStorage } from "@/utils/auth";
 
 const config: AxiosRequestConfig = {
-  timeout: 5 * 1000, // 超时设置(单位毫秒)，无超时时间设置为 0。
+  timeout: 20 * 1000, // 超时设置(单位毫秒)，无超时时间设置为 0。
   responseType: "json", // 响应的数据格式：json/blob/document/arraybuffer/text/stream
   headers: {
     "Content-Type": "application/json",
